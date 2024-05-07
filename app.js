83bed9c4846759e34a56f25bccb934f2f099a22e
helloPankajViswakarma <156342181+helloPankajViswakarma@users.noreply.github.com>
2024-05-07 18:35:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const BASE_URL =
 
 const dropdowns = document.querySelectorAll(".dropdown select")
   
-// const dropdowns = document.querySelectorAll(".dropdown select");
-// const btn = document.querySelector("form button");
-// const fromCurr = document.querySelector(".from select");
-// const toCurr = document.querySelector(".to select");
-// const msg = document.querySelector(".msg");
+ const dropdowns = document.querySelectorAll(".dropdown select");
+ const btn = document.querySelector("form button");
+ const fromCurr = document.querySelector(".from select");
+ const toCurr = document.querySelector(".to select");
+ const msg = document.querySelector(".msg");
 
  for (let select of dropdowns) {
    for (currCode in countryList) {
@@ -28,35 +28,35 @@ const dropdowns = document.querySelectorAll(".dropdown select")
    });
  }
 
-// const updateExchangeRate = async () => {
-//   let amount = document.querySelector(".amount input");
-//   let amtVal = amount.value;
-//   if (amtVal === "" || amtVal < 1) {
-//     amtVal = 1;
-//     amount.value = "1";
-//   }
-//   const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-//   let response = await fetch(URL);
-//   let data = await response.json();
-//   let rate = data[toCurr.value.toLowerCase()];
+ const updateExchangeRate = async () => {
+   let amount = document.querySelector(".amount input");
+   let amtVal = amount.value;
+   if (amtVal === "" || amtVal < 1) {
+     amtVal = 1;
+     amount.value = "1";
+  }
+   const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+   let response = await fetch(URL);
+   let data = await response.json();
+   let rate = data[toCurr.value.toLowerCase()];
 
-//   let finalAmount = amtVal * rate;
-//   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-// };
+   let finalAmount = amtVal * rate;
+   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+ };
 
-// const updateFlag = (element) => {
-//   let currCode = element.value;
-//   let countryCode = countryList[currCode];
-//   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-//   let img = element.parentElement.querySelector("img");
-//   img.src = newSrc;
-// };
+ const updateFlag = (element) => {
+   let currCode = element.value;
+   let countryCode = countryList[currCode];
+   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+   let img = element.parentElement.querySelector("img");
+   img.src = newSrc;
+ };
 
-// btn.addEventListener("click", (evt) => {
-//   evt.preventDefault();
-//   updateExchangeRate();
-// });
+ btn.addEventListener("click", (evt) => {
+   evt.preventDefault();
+   updateExchangeRate();
+ });
 
-// window.addEventListener("load", () => {
-//   updateExchangeRate();
-// });
+ window.addEventListener("load", () => {
+   updateExchangeRate();
+ });
